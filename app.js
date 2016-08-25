76aead6626a7e9a3f676085b9a7dd884d486f475
sammia28 <sammia.zubizarreta@gmail.com>
2016-08-25 15:21:04 -0500

    corregi texto

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
 var num1 = prompt ("¿Cuánto ganas por hora?");
 var num2 = prompt ("¿Cuántas horas trabajas a la semana?");
 var suma = parseInt(num1) * parseInt(num2);
-    window.alert (suma);
+window.alert (suma);
